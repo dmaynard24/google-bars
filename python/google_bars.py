e6dc8128816326e8fd2c078df09f696e88af15a8
Dave Maynard <dmayna@amazon.com>
2022-01-30 10:32:49 -0500

    updated columns and changed the queries to be Search

--- python/google_bars.py
+++ python/google_bars.py
@@ -9,6 +9,7 @@ gmaps = googlemaps.Client(key=config('API_KEY'))
 
 field_names = {
     'Name of Bar': 'name',
+    'Vicinity': 'vicinity',
     'Full Address': 'formatted_address',
     'Phone Number': 'formatted_phone_number',
     'Website': 'website',
@@ -16,8 +17,15 @@ field_names = {
     'Rating': 'rating',
     'Total User Ratings': 'user_ratings_total',
     'Google Maps Page': 'url',
+    'Business Status': 'business_status',
     'Hours of Operation': 'opening_hours'
 }
+
+excel_cols = list(field_names.keys())[0:len(field_names) - 1]
+excel_cols.extend([
+    'Monday Hours', 'Tuesday Hours', 'Wednesday Hours', 'Thursday Hours',
+    'Friday Hours', 'Saturday Hours', 'Sunday Hours'
+])
 
 cities = [
     'Asheville, NC', 'Atlanta, GA', 'Austin, TX', 'Chicago, IL', 'Dallas, TX',
@@ -25,7 +33,7 @@ cities = [
     'New Orleans, LA', 'New York, NY', 'San Francisco, CA'
 ]
 
-num_cols = len(field_names)
+num_cols = len(excel_cols)
 num_rows = 61
 
 
@@ -49,28 +57,46 @@ def places_in_city(city, city_geocode, radius=24140):
 
   places = []
   print('Retrieving page 1 for {}...'.format(city))
-  page = gmaps.places_nearby(location=city_geocode, radius=radius, type='bar')
+  page = gmaps.places(query=f'best local bars in {city}',
+                      location=city_geocode,
+                      radius=radius,
+                      type='bar')
   places.extend(places_details(page['results']))
   i = 0
   while 'next_page_token' in page and i < 4:
     time.sleep(2)
     print('Retrieving page {} for {}...'.format(i + 2, city))
-    page = gmaps.places_nearby(location=city_geocode,
-                               radius=radius,
-                               type='bar',
-                               page_token=page['next_page_token'])
+    page = gmaps.places(query=f'best local bars in {city}',
+                        location=city_geocode,
+                        radius=radius,
+                        type='bar',
+                        page_token=page['next_page_token'])
     places.extend(places_details(page['results']))
     i += 1
   return places
 
 
 def desired_fields(place):
-  desired_place = {
-      key: place.get(val, '')
-      for key, val in field_names.items() if val != 'opening_hours'
-  }
-  desired_place['Hours of Operation'] = ', '.join(
-      place.get('opening_hours', {}).get('weekday_text', []))
+  only_hours = lambda place_hours: place_hours[place_hours.find(':') + 2:]
+
+  desired_place = {}
+
+  # handle all other fields besides hours
+  for key, val in field_names.items():
+    if val != 'photo' and val != 'opening_hours':
+      desired_place[key] = place.get(val, '')
+
+  # handle hours
+  place_hours = place.get('opening_hours', {}).get('weekday_text', [])
+  if len(place_hours) == 7:
+    desired_place['Monday Hours'] = only_hours(place_hours[0])
+    desired_place['Tuesday Hours'] = only_hours(place_hours[1])
+    desired_place['Wednesday Hours'] = only_hours(place_hours[2])
+    desired_place['Thursday Hours'] = only_hours(place_hours[3])
+    desired_place['Friday Hours'] = only_hours(place_hours[4])
+    desired_place['Saturday Hours'] = only_hours(place_hours[5])
+    desired_place['Sunday Hours'] = only_hours(place_hours[6])
+
   return desired_place
 
 
@@ -88,7 +114,7 @@ def get_geocodes_and_write_excel():
     for col in range(num_cols):
       cell = ws[f'{get_column_letter(col + 1)}1']
       cell.font = Font(bold=True)
-      cell.value = list(field_names.keys())[col]
+      cell.value = excel_cols[col]
 
     bars = list(map(desired_fields, places_in_city(city, city_geocodes[i])))
 
